214fc4579581f9ab2de584cae6dc8eddbdf7b5cd
portalplayer <kratcy@fuckb.ing>
2026-04-29 23:40:31 -0400

    source: Added help message

--- source.js
+++ source.js
@@ -1,15 +1,24 @@
 // Purpose: Main index
 // Created on: 4/29/26 @ 11:10 PM
 
+const {basename} = require("path")
+
 const main = async () => {
+    if (process.argv.length <= 2) {
+        const executableName = basename(process.argv[1])
+
+        console.log(`usage:  ${executableName} <operation> [...]`)
+        console.log("operations:")
+        console.log(`    ${executableName} {-I --install}     <mod>`)
+        console.log(`    ${executableName} {-S --search}     <mod>`)
+        console.log(`    ${executableName} {-L --list}`)
+        return
+    }
+
     // TODO: More than just Left 4 Dead 2?
     console.log("Getting mods")
 
     const mods = await (await fetch("http://10.0.44.20:5113/Mods/Left 4 Dead 2/data.json")).json()
-
-    console.log(`Loaded: ${mods.response.resultcount} mods`)
-
-
 }
 
 main()
