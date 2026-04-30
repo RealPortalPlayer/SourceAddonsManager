5342f9a089ea85634c0ea7781b8f6b75a867f325
portalplayer <kratcy@fuckb.ing>
2026-04-29 23:29:49 -0400

    source: Get `data.json`

--- source.js
+++ source.js
@@ -1,4 +1,15 @@
 // Purpose: Main index
 // Created on: 4/29/26 @ 11:10 PM
 
-console.log("Hello, World!")
+const main = async () => {
+    // TODO: More than just Left 4 Dead 2?
+    console.log("Getting mods")
+
+    const mods = await (await fetch("http://10.0.44.20:5113/Mods/Left 4 Dead 2/data.json")).json()
+
+    console.log(`Loaded: ${mods.response.resultcount} mods`)
+
+
+}
+
+main()
