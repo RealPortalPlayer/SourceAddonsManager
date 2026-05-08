3eeb981a7029deaf87010e3c9b609adc5555db99
portalplayer <kratcy@fuckb.ing>
2026-05-08 06:27:00 -0400

    collection_add: Automatically install addons for enabled collections

--- argument/collection_add.js
+++ argument/collection_add.js
@@ -2,7 +2,15 @@
 // Created on: 5/1/26 @ 11:04 AM
 
 const Collections = require("../internal/collections")
+const Addons = require("../internal/addons")
 
-module.exports = require("../internal/argument")("Add to a local collection", ["<name>", "<addon>", "[--override]"], () => {
+module.exports = require("../internal/argument")("Add to a local collection", ["<name>", "<addon>", "[--override]"], async () => {
     Collections.addLocal(process.argv[4], process.argv[5], process.argv[6] === "--override")
+
+    const collection = Collections.get(process.argv[4])
+
+    if (!Collections.getEnabled().includes(collection))
+        return
+
+    await Addons.install(Addons.find(process.argv[5], false)[0])
 })
