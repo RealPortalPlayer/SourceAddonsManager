1926b893c560214857b994a3978366ccaa2e2cd9
portalplayer <kratcy@fuckb.ing>
2026-05-09 19:04:26 -0400

    stats: Tell the number of addons that were deleted off Workshop

--- argument/stats.js
+++ argument/stats.js
@@ -5,8 +5,20 @@ const Logger =  require("../internal/logger")
 const Manager = require("../internal/manager")
 
 module.exports = require("../internal/argument")("Show stats", [], () => {
+    const addons = Manager.getAllAddons()
+    let dead = 0
+
+    for (const addon of addons) {
+        if (addon.result === 1)
+            continue
+
+        dead++
+    }
+
     Logger.debug(Manager.getAllAddons().length)
     Logger.debug(Manager.getAllCollections().length)
-    Logger.log(`Addons: ${Manager.getAllAddons().length}`)
+    Logger.log(`Addons: ${addons.length} (${dead} of which were deleted off the Workshop)`)
     Logger.log(`Collections: ${Manager.getEnabledCollections().length}/${Manager.getAllCollections().length}`)
+
+
 })
